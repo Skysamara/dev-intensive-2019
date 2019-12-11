ae7f3bddf79ef09e374d280fe555594d3f2d8658
skysamara <skysamara@gmail.com>
2019-12-11 17:57:24 +0300

    hometask_3. Ошибка при неправильнос ответе

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,16 +1,52 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.view.Window
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var benderImage : ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("My_MainActivity.kt","onCreate")
+
+//        benderImage = findViewById(R.id.iv_bender) as ImageView
+//        benderImage = findViewById<ImageView>(R.id.iv_bender)
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+        val status = savedInstanceState?.getString("STATUS") ?:Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?:Bender.Question.NAME.name
+
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        Log.d("My_MainActivity.kt","onCreate $status $question")
+
+        val (r,g,b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+
+//        textTxt.setText(benderObj.askQuestion())
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+
     }
 
     override fun onStart() {
@@ -48,11 +84,26 @@ class MainActivity : AppCompatActivity() {
         Log.d("My_MainActivity.kt", "onRestoreInstanceState")
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        Log.d("My_MainActivity.kt", "onSaveInstanceState")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION", benderObj.question.name)
+        Log.d("My_MainActivity.kt","onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+
     }
 
+    override fun onClick(v: View?) {
+        //TODO Ошибка при неправильно ответе
+        //https://www.youtube.com/watch?v=cJaatOwP_WA&feature=youtu.be&t=6163
+        if (v?.id == R.id.iv_send){
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r,g,b) = color
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+        }
+    }
 
 
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -11,25 +11,55 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
         Question.IDLE -> Question.IDLE.question
     }
 
-    fun listnAnswer(answer:String):Pair<String, Triple<Int, Int, Int>>{
-
+    fun listenAnswer(answer:String):Pair<String, Triple<Int, Int, Int>>{
         //TODO https://youtu.be/cJaatOwP_WA?t=6163
 
+        return if (question.answer.contains(answer)){
+            question = question.nextQuetion()
+            "Отлично - это правильный ответ!\n${question.question}" to status.color
+        } else{
+            status = status.nextStatus()
+            "Это неправильный ответ!\n${question.nextQuetion()}" to status.color
+
+        }
     }
 
     enum class Status(val color : Triple<Int, Int, Int>){
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0)),
+        CRITICAL(Triple(255,255,0));
+
+        fun nextStatus(): Status{
+            return  if (this.ordinal < values().lastIndex){
+                values()[this.ordinal + 1]
+            }
+            else{
+                values()[0]
+            }
+        }
     }
 
     enum class Question(val question: String, val answer:List<String>){
-        NAME("Как меня зовут?", listOf("бендер", "bender")),
-        PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")),
-        MATERIAL("Из чго я сделан?", listOf("металл", "дерево","iron","wood","metal")),
-        BDAY("Когда меня создали?", listOf("2993")),
-        SERIAL("Мой серийный номе?", listOf("2716057")),
-        IDLE("Наэтом всё, вопросов больше нет", listOf())
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuetion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию", listOf("сгибальщик", "bender")) {
+            override fun nextQuetion(): Question = MATERIAL
+        },
+        MATERIAL("Из чго я сделан?", listOf("металл", "дерево","iron","wood","metal")) {
+            override fun nextQuetion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuetion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номе?", listOf("2716057")) {
+            override fun nextQuetion(): Question = IDLE
+        },
+        IDLE("Наэтом всё, вопросов больше нет", listOf()) {
+            override fun nextQuetion(): Question = IDLE
+        };
+
+        abstract fun nextQuetion():Question
     }
 }
