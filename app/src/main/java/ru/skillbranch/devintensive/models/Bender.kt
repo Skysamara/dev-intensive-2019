20be33853a12b59b5ba39a01e53494e430dfad9e
Skysamara <skysamara@gmail.com>
2019-12-12 22:56:18 +0300

    Основное. 3 задание.

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -27,22 +27,14 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
             return "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
         }
 
-//        if (question.isAnswerCorrect(answer)){
-//            question = question.nextQuetion()
-//            Log.d("My_Bender.kt","$status ${status.color}")
-//            return "Отлично - ты справился\n${question.question}" to status.color
-//        } else
-//        {
-//            status = status.nextStatus()
-//            Log.d("My_Bender.kt","$status ${status.color}")
-//            return "Это неправильный ответ!\n${question.question}" to status.color
-//        }
         return if (question.answer.contains(answer)){
             question = question.nextQuetion()
+            Log.d("My_Bender.kt","$status ${status.color}")
             "Отлично - ты справился\n${question.question}" to status.color
         } else{
             status = status.nextStatus()
             question.nextQuetion()
+            Log.d("My_Bender.kt","$status ${status.color}")
             "Это неправильный ответ\n${question.question}" to status.color
         }
     }
@@ -51,7 +43,7 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        CRITICAL(Triple(255,0,0));
 
         fun nextStatus(): Status{
             return  if (this.ordinal < values().lastIndex){
